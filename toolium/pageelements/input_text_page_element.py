0e3977603f0a6e78896783d5e8aa265693f15b4e
hesteban-tuenti <hector.estebanramos@telefonica.com>
2024-10-25 13:30:41 +0200

    set_text

--- toolium/pageelements/input_text_page_element.py
+++ toolium/pageelements/input_text_page_element.py
@@ -40,7 +40,8 @@ class InputText(PageElement):
         :param value: value to be set
         """
         if self.driver_wrapper.is_ios_test() and not self.driver_wrapper.is_web_test():
-            self.web_element.send_keys(value)
+            self.web_element.set_text(value)
+            # self.web_element.send_keys(value)
         elif self.shadowroot:
             value = value.replace("\"", "\\\"")
             self.driver.execute_script('return document.querySelector("%s")'
